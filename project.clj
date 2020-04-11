5699473fdb82030b01beb18d87ba43e47ed726a1
Jannik Luhn <jannik@brainbot.com>
2020-04-11 09:50:14 +0200

    Prepare 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject low-disc "0.1.0-SNAPSHOT"
+(defproject low-disc "0.1.0"
   :description "Low-discrepancy sequences"
   :url "http://github.com/absentpig/low-disc"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
